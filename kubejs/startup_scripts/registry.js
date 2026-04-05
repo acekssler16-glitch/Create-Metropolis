3e7d848d9977f18b84d7dcc367a59cd1d7fb12a0
acekssler16-glitch <acekssler16@gmail.com>
2026-04-05 15:24:16 -0500

    Rebased Dev Onto Main
    Additionally fixed crash.

--- kubejs/startup_scripts/registry.js
+++ kubejs/startup_scripts/registry.js
@@ -2,4 +2,13 @@
 console.info('Items Registered! (Loaded startup registry script)')
 StartupEvents.registry('item', event => {
   event.create('shimmer')
+// Ween
+event.create('godweensatan').displayName('GodWeenSatan: The Oneness')
+event.create('the_pod').displayName('The Pod')
+event.create('pure_guava').displayName('Pure Guava')
+event.create('chocolate_and_cheese').displayName('Chocolate & Cheese')
+event.create('twelve_country_greats').displayName('12 Golden Country Greats')
+event.create('the_mollusk').displayName('The Mollusk')
+event.create('white_pepper').displayName('White Pepper')
+event.create('quebec')
 })
